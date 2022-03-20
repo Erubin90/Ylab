45affc314c041059b943a25688e3d131191f1fa1
Erubin <magomedamin90@ya.ru>
2022-03-20 18:33:49 +0300

    Добавлен сырой метод processFile()

--- src/main/java/io/ylab/ticTacToeGame/controlers/SimulationController.java
+++ src/main/java/io/ylab/ticTacToeGame/controlers/SimulationController.java
@@ -1,32 +1,51 @@
 package io.ylab.ticTacToeGame.controlers;
 
-import io.ylab.ticTacToeGame.exceptions.StepNoCorrectValueException;
-import io.ylab.ticTacToeGame.model.GameAnswer;
-import io.ylab.ticTacToeGame.model.GamePlayModel;
+import io.ylab.ticTacToeGame.objects.enums.FileFormat;
+import io.ylab.ticTacToeGame.parsers.gameParsers.GameParser;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
 
 @RestController
 @RequestMapping("/simulation/play")
 class SimulationController {
 
-    @PostMapping("/json")
-    public void processJson(@RequestBody GameAnswer answer) {
-        try {
-            answer.getGame().play();
-        } catch (StepNoCorrectValueException e) {
-            e.printStackTrace();
+    @PostMapping("/file")
+    public String processFile(@RequestBody MultipartFile answer) {
+        var fileName = answer.getOriginalFilename();
+        if (fileName != null) {
+            int index = fileName.lastIndexOf(".");
+            var format = fileName.substring(++index);
+            var fileFormat = FileFormat.getFileFormat(format);
+            GameParser parser;
+            switch (fileFormat) {
+                case JSON:
+                case XML:
+                    parser = new GameParser(fileFormat);
+                    break;
+                default:
+                    return "Формат файла \""+ format +"\" не поддерживается";
+            }
+            String file;
+            try {
+                file = new String(answer.getInputStream().readAllBytes());
+            } catch (IOException e) {
+                return "Не получилось прочитать файл";
+            }
+            try {
+                var game = parser.read(file);
+                game.play();
+            } catch (IOException e) {
+                return "Содержимое файла не соответствует шаблону";
+            }
         }
-    }
-
-    @PostMapping("/xml")
-    public void processXml(@RequestBody GamePlayModel answer) {
-        try {
-            answer.getGame().play();
-        } catch (StepNoCorrectValueException e) {
-            e.printStackTrace();
+        else {
+            return "Тело не содержит файла";
         }
+        return "Игра воспроизвелась";
     }
 }
